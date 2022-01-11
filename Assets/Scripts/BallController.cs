5bf7222a4e563e5f88f70fc562c9f1bad472c1cf
goya5858 <hokuzaki1203@gmail.com>
2022-01-11 11:40:01 +0900

    first commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myRigidbody = GetComponent<Rigidbody>();
+        this.myRigidbody.velocity = new Vector3(speed, speed, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Vector3 f_position;
+    Vector3 m_position;
+    float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.f_position = this.myRigidbody.position;
+        this.m_position = Input.mousePosition; //マウスの位置を取得
+        y = Mathf.Clamp( (this.m_position.y/20.0f - 7.0f), -5.0f, 5.0f);
+        this.f_position.y = y; //マウスの位置を
+
+        this.myRigidbody.position = this.f_position;
+    }
+}
